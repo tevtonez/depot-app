b5b4a8fbbc32758f71d92e61ac57fef7056d9cbb
tevtonez <tevtonez@gmail.com>
2015-03-01 09:12:03 +0100

    F2: Creating an Ajax-Based Cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -35,6 +35,7 @@ class LineItemsController < ApplicationController
       if @line_item.save
         session[:counter] = 0
         format.html { redirect_to store_url }
+        format.js
         format.json { render action: 'show', status: :created, location: @line_item }
       else
         format.html { render action: 'new' }
